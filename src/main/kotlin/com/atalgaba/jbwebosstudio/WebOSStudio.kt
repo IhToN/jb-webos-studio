31e84dc5592854bb9a18f93615d60e4aa304ecc5
ATAlgaba <atalgaba@gmail.com>
2024-05-10 17:35:41 +0200

    feat: Add a notification handler
    Signed-off-by: ATAlgaba <atalgaba@gmail.com>

--- src/main/kotlin/com/atalgaba/jbwebosstudio/WebOSStudio.kt
+++ src/main/kotlin/com/atalgaba/jbwebosstudio/WebOSStudio.kt
@@ -1,9 +1,14 @@
 package com.atalgaba.jbwebosstudio
 
 import com.intellij.DynamicBundle
+import com.intellij.notification.*
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
+import javax.swing.Icon
 
 @NonNls
 private const val BUNDLE = "messages.WebOSStudio"
@@ -19,6 +24,60 @@ object WebOSStudio : DynamicBundle(BUNDLE) {
     @JvmStatic
     fun messagePointer(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
         getLazyMessage(key, *params)
+
+    @JvmStatic
+    fun showNotification(type: NotificationType, title: String, content: String, project: Project? = null) {
+        val notificationGroup = NotificationGroup.findRegisteredGroup("webOSStudio.BalloonGroup")
+        if (notificationGroup != null) {
+            val notification = notificationGroup.createNotification(title, content, type)
+            if (project != null) {
+                notification.notify(project)
+            } else {
+                Notifications.Bus.notify(notification)
+            }
+        }
+    }
+
+    @JvmStatic
+    fun showNotification(type: NotificationType, title: String, content: List<String>, project: Project? = null) {
+        val notificationGroup = NotificationGroup.findRegisteredGroup("webOSStudio.BalloonGroup")
+        if (notificationGroup != null) {
+            val notificationContent = content[0] // Concatenate the list of strings into a single string
+            val notification = notificationGroup.createNotification(title, notificationContent, type)
+
+            // Add a notification listener to show details
+            val showDetailsAction = object : NotificationAction("Show Details") {
+                override fun actionPerformed(e: AnActionEvent, notification: Notification) {
+                    val details = content.joinToString("\n") // Concatenate the list of strings into a single string
+                    val icon: Icon = when (type) {
+                        NotificationType.WARNING -> {
+                            Messages.getWarningIcon()
+                        }
+
+                        NotificationType.ERROR -> {
+                            Messages.getErrorIcon()
+                        }
+
+                        NotificationType.INFORMATION -> {
+                            Messages.getInformationIcon()
+                        }
+
+                        NotificationType.IDE_UPDATE -> {
+                            Messages.getInformationIcon()
+                        }
+                    }
+                    Messages.showMessageDialog(details, title, icon)
+                }
+            }
+            notification.addAction(showDetailsAction)
+
+            if (project != null) {
+                notification.notify(project)
+            } else {
+                Notifications.Bus.notify(notification)
+            }
+        }
+    }
 
     @JvmStatic
     fun isWebOSTemplateDirectory(directory: VirtualFile?): Boolean {
