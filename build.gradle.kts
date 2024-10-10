e33627262036d7e865c6e4ac72a96c77c45342db
ATAlgaba <atalgaba@gmail.com>
2024-10-10 14:28:45 +0200

    chore: change headerParserRegex for gradle-changelog-plugin
    Issue: #12
    Signed-off-by: ATAlgaba <atalgaba@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,6 +48,7 @@ intellij {
 changelog {
     groups.empty()
     repositoryUrl = properties("pluginRepositoryUrl")
+    headerParserRegex.set("""((0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)""".toRegex())
 }
 
 // Configure Gradle Kover Plugin - read more: https://github.com/Kotlin/kotlinx-kover#configuration
